07822f3d24bfdec3b769687301b918ceab3d2d41
raffii342 <79618538+raffii342@users.noreply.github.com>
2021-02-26 07:36:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://sore-cooperative-ball.glitch.me"]
+const urls = ["https://steadfast-abaft-decimal.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
